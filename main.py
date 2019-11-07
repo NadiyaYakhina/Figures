edb6763b1185e968f1b5d58c0fdaa0e1847ad99d
Nadya Yakhina <tab.naya05@gmail.com>
2019-11-07 17:03:37 +0700

    Inital version

--- main.py
+++ main.py
@@ -1,1 +1,17 @@
 # This is our project
+import turtle
+# wn = turtle.Screen()
+# wn.title("My Turtle")
+pen = turtle.Turtle()
+pen.screen.title("My Turtle 2")
+
+# начинаем рисовать тут
+
+# lets draw square
+
+# then draw the triangle
+
+# then draw spiral
+
+
+pen.screen.exitonclick()
